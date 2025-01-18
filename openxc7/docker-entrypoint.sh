c845ef0c4f9f57401f2d63ed42d1429a5cacc2e3
sangibeaud <stephane.angibeaud@gmail.com>
2025-01-18 16:47:38 +0100

    Gosu user with environment variables

--- openxc7/docker-entrypoint.sh
+++ openxc7/docker-entrypoint.sh
@@ -2,4 +2,16 @@
 
 source /prjxray/env/bin/activate
 
-exec "$@"
+# Check if USER_ID and GROUP_ID environment variables are set
+if [ ! -z "$USER_ID" ] && [ ! -z "$GROUP_ID" ]; then
+    # Create group and user
+    groupadd -g "$GROUP_ID" usergroup
+    #useradd -h
+    useradd -u "$USER_ID" -g usergroup -m user -s /usr/bin/sh -d /home/user
+    #cp /etc/skel/.* /home/user/
+
+    # Execute command using gosu
+    exec gosu user "$@"
+else
+    exec "$@"
+fi
